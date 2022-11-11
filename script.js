cffa825676b63349bc28c6c05ec4535fc0891449
Miss-Google <108806577+Miss-Google@users.noreply.github.com>
2022-11-11 23:45:50 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+// console.log("Hello");
+
+// quws2
+//  var a=prompt("name ?");
+// console.log(a);
+
+// ques3
+// var a=Number(prompt("1st number"));
+// var b=Number(prompt("2nd number"));
+// console.log("The sum of two number is : "+(a+b));
+
+// ques4
+// var a=prompt("you want to go out");
+// if(a=='yes'){
+//     console.log("go");
+// }
+// else{
+//     console.log("Don't go");
+
+// }
+
+// ques5
+// for(var i=1;i<=50;i++){
+//     console.log("Hello")
+// }
+const scroll = new LocomotiveScroll({
+    el: document.querySelector("#main"),
+    smooth: true
+});
